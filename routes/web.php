cd564dbee82afd4bbabce0ac23e6cd41cefc6fc2
hamzazirari <hamzazirari6002@gmail.com>
2026-01-29 10:22:44 +0100

    feat : add create recipe route

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+-- Active: 1769523556450@@127.0.0.1@5432@recipes_db
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -8,8 +9,7 @@ Route::get('/acceuil', function () {
     return '<h1>Ma plateforme de recettes</h1>';
 });
 
-Route::get('/', function () {
-    return '<h1>Ma plateforme de recettes</h1>';
-});
 
 Route::get('/', [RecipeController::class, 'index']);
+
+Route::get('/recipes/create',[RecipeController::class, 'create']);
